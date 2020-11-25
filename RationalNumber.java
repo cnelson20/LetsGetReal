946f72d908fe8a3f7b67153cca29d446508adb75
cnelson20 <cjnelson2004@gmail.com>
2020-11-25 12:37:29 -0500

    updated the RationalNumber constructor

--- RationalNumber.java
+++ RationalNumber.java
@@ -44,6 +44,7 @@ public class RationalNumber extends RealNumber
 
 
   public String toString(){
+    if (getNumerator() == 0) {return "0";}
     if (getDenominator() == 1) {
       return getNumerator() + "";
     }
